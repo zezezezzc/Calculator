7d8f8cbe787baaa9a26945e1ab478908f078f5d7
zezeze.Zhang <43145314+zezezezzc@users.noreply.github.com>
2020-07-20 18:08:13 +0800

    Update T1.cpp
    修改1

--- T1.cpp
+++ T1.cpp
@@ -12,54 +12,33 @@ using std::string;
 
 void caculate(vector<float>& v1, vector<char>& v2, int i, bool flg)	//完成一次计算
 {
-	if (flg) {
-		int t = i-2;	 //检查移一个符号是否为- 
-		if (t>=0) {
-			switch (v2[t])
-			{
-				case '+': v1[i-1] -= v1[i]; break;
-				case '-': v1[i-1] += v1[i]; break;	//更改 + - 
-				case '*': v1[i-1] *= v1[i]; break;
-				case '/': v1[i-1] /= v1[i]; break;
-			}
-		}
-		else {
-			switch (v2[i-1])
-			{
-				case '+': v1[i-1] += v1[i]; break;
-				case '-': v1[i-1] -= v1[i]; break;
-				case '*': v1[i-1] *= v1[i]; break;
-				case '/': v1[i-1] /= v1[i]; break;
-			}
+	int t = i;
+	if (!flg) {
+		--i;	//无括号时偏移1位;	因为有括号时，符号容器多了一个 ( 
+		--t;	//无括号时，t同步偏移 
+	}
+	else
+		--t;	//有括号时与i相差1 
+	
+	if((i-1)>=0) {
+		if (v2[i-1]=='-') {	//判断是否变号 
+			if (flg) {	//有括号，对应数字容器差一个下标 
+				v1[i] = (-1)*v1[i];
+			} 
+			else
+				v1[i+1] = (-1)*v1[i+1];		//若前一符号为-，则更改最后一个计算数的符号
 		}
 	}
-	else {
-		switch (v2[i-1])
-		{
-			case '+': v1[i-1] += v1[i]; break;
-			case '-': v1[i-1] -= v1[i]; break;
-			case '*': v1[i-1] *= v1[i]; break;
-			case '/': v1[i-1] /= v1[i]; break;
-		}	
-	}	
-	v1.pop_back();
-	v2[i-1] = v2[i];	//符号前移一个
-	v2.pop_back();
-}
-
-void simplify(vector<float>& v1, vector<char>& v2, int i)	//完成一次化简 
-{
 	switch (v2[i])
 	{
-		case '+': v1[i-1] += v1[i]; break;
-		case '-': v1[i-1] -= v1[i]; break;
-		case '*': v1[i-1] *= v1[i]; break;
-		case '/': v1[i-1] /= v1[i]; break;
-	}	
-	
+		case '+': v1[t] += v1[t+1]; break;
+		case '-': v1[t] -= v1[t+1]; break;
+		case '*': v1[t] *= v1[t+1]; break;
+		case '/': v1[t] /= v1[t+1]; break;
+	}
 	v1.pop_back();
-	v2[i] = v2[i+1];	//括号前移一个
-	v2.pop_back();	//删除一个旧的 
+	v2[i] = v2[i+1];	//符号前移一个
+	v2.pop_back();
 }
 
 int main() {
@@ -79,16 +58,15 @@ int main() {
 		fnum.clear();
 		csign.clear();
 		/* 进行计算 */
-		for (auto c : cacul) {		//左、右括号() 会遇到两个符号连在一起 
+		for (auto c : cacul) {		//左、右括号() 会遇到两个符号连在一起
 			if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') {
 				
-				if (!s.empty()) {	//先输入为符号
+				if (!s.empty()) {
 					x = stof(s);	//转换为浮点数
 					fnum.push_back(x);
 					s = "";		//清空s中存的数字
 					index = fnum.size() - 1;	//符号vector中即将存入元素的下标
 				}
-
 				if (c == '(') {
 					csign.push_back(c);
 					continue;
@@ -96,7 +74,7 @@ int main() {
 				if (c == ')') {
 					csign.push_back(c);
 					while (csign[index] != '(') {
-						simplify(fnum, csign, index);
+						caculate(fnum, csign, index, true);
 						index = fnum.size() - 1;	//重新计算下标
 					}
 					csign.pop_back();
@@ -104,13 +82,13 @@ int main() {
 					continue;
 				}
 				if (c == '*' || c == '/') {
-					csign.push_back(c);					
+					csign.push_back(c);
 				}
 				else {
 					if (index == 0) {
 						csign.push_back(c);		//存入 + 或 -
 					}
-					else {						
+					else {
 						csign.push_back(c);		//存入 + 或 -
 						if (csign[index] != '(') {	//要检测是否在括号内 
 							while (csign[index-1] == '*' || csign[index-1] == '/') {
@@ -125,22 +103,21 @@ int main() {
 				s += c;
 			}
 		}
-		if (!s.empty()) {//若式中最后为数字将其转换为浮点数
+		if (!s.empty()) {//若输入式中最后为数字将其转换为浮点数
 			x = stof(s);	
 			fnum.push_back(x);
 			s = "";		//清空s中存的数字
 			index = fnum.size() - 1;	//符号vector中即将存入元素的下标
 		}
-				
+
 		while (!csign.empty()) {	//从右至左进行计算
 			index = fnum.size() - 1;	//计算式中计算元素的下标
-			caculate(fnum, csign, index, true);
+			caculate(fnum, csign, index, false);
 		}
 
 		res = fnum[0];
 		cout << "计算结果为：" << res << '\n' <<"敲击回车即可退出或输入算式：" << endl;
 	}
-
 	return 0;
 }
 
